0ff8f243b07719bc3b513e59fd70e9cc51e25cc8
itenyh <mkeqi@mkedeMacBook-Pro.local>
2018-07-22 16:51:38 +0800

    修复一个declaration导致的问题

--- Convertor/main.js
+++ Convertor/main.js
@@ -2,13 +2,11 @@ require('./JPConvertor')
 var fs = require('fs');
 
 // var data = fs.readFileSync('/Users/iten/iten的文档/Code/SmallCity/SmallCity/iCMCity/HeatMapModule/View/iCMCityMapView.m', 'utf8');
-// var data = "(NSObject *)[UIView new];";
-var data = "for (int i = 0;i < 10;i++) { \n" +
-            "}";
+var data = "((NSObject *)[[UIView new] new]);";
 
 var t1 = new Date().getTime();
 convertor(data, function (result, className, err) {
-    // console.log(result, className, (err == undefined) ? '' : err);
+    console.log(result, className, (err == undefined) ? '' : err);
     // console.log('Time Used: ' + (new Date().getTime() - t1));
 });
 
