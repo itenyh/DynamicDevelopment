52142ed8c7183668ce644ef70514a4e537867d5d
itenyh <itenyh@itenyhdeMac-mini.local>
2018-08-06 22:14:36 +0800

    临时提交

--- Demo/Demo/AppDelegate.m
+++ Demo/Demo/AppDelegate.m
@@ -47,3 +47,31 @@
 }
 
 @end
+
+/*
+ 2017.12 - now
+ Working at China Mobile Communications Corporation of ChongQing
+ position: Mobile App Engineer (iOS)
+ duty: 1. Develop new modules for existing app
+ 2. Find and Repair existing bugs and refractor old code
+ 3. Code review for the other two iOS developers
+ project:
+ 1. ChongQing City: https://itunes.apple.com/cn/app/%E9%87%8D%E5%BA%86%E5%9F%8E/id595252184?mt=8
+ The architecture of the existing modules are MVC, which leads to massive controller, logical code couples with view module, network requests are placed in model parts, etc. I try to write new code in MVVM pattern, decouple model, view, logical, network from each other and ReactiveCocoa helps me much.
+ 
+ 2. Hedgehog
+ A personal project which is still in developing state, but it has played a role in my daily coding. The direct reason for starting the project is that my company gave me an old machine, so that compiling always take a long time. Facilitated by the runtime ability of Objective-C, Hedgehog can help to load new modules while the program is runing which make developing much more effective.
+ 
+ Working at Qimke Technology Co Ltd
+ position: Mobile App Engineer (iOS); Venture partner;
+ duty: 1. Totally in charge of app development on iphone
+ 2. Contribute all aspects of ideas and make them come true
+ project:
+ QiHua, recording your photos, path, mood during your traveling and the most import, you can collaberate with your partners.
+ 1. Adopting Swift as programming language.
+ 2. To deal with the large project and complex logic, MVVM was sticked. Compared with RxSwift to realize Responsive programming.
+ 3. Too much markers on map causes much pressure to GPU which leads to unsmooth view movement. To solve the problem, I use CoreGraphic to draw the markers on the background thread.
+ 
+ 
+ 
+ */
